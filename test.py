7060819a8fddec227fefd9a7c9885bd016aadd2a
dalmoon <34093146+dalmoon@users.noreply.github.com>
2021-06-22 11:16:36 +0900

    Update test.py

--- test.py
+++ test.py
@@ -30,9 +30,9 @@ while True:
                 b = upbit.get_order("KRW-ETH")[1]["uuid"] # 매도 UUID
                 #print("uuid 2개")
             else:
-                a = upbit.get_order("KRW-ETH", state="done")[0]["uuid"]
+                b = upbit.get_order("KRW-ETH", state="done")[0]["uuid"]
                 if 1 == len(upbit.get_order("KRW-ETH")):
-                    b = upbit.get_order("KRW-ETH")[0]["uuid"] # 매수 UUID
+                    a = upbit.get_order("KRW-ETH")[0]["uuid"] # 매수 UUID
                     #print("바로 체결?")
         elif b == upbit.get_order("KRW-ETH")[0]["uuid"]:
             #print("b에 들어옴")
@@ -50,8 +50,8 @@ while True:
                 b = upbit.get_order("KRW-ETH")[1]["uuid"] # 매도 UUID
                 #print("uuid 2개")
             else:
-                a = upbit.get_order("KRW-ETH", state="done")[0]["uuid"]
+                b = upbit.get_order("KRW-ETH", state="done")[0]["uuid"]
                 if 1 == len(upbit.get_order("KRW-ETH")):
-                    b = upbit.get_order("KRW-ETH")[0]["uuid"] # 매수 UUID
+                    a = upbit.get_order("KRW-ETH")[0]["uuid"] # 매수 UUID
                     #print("바로 체결?")
     #time.sleep(0.001)
