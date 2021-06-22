60878e1927f83a5f9ce3a5d543e034b989d96160
dalmoon <34093146+dalmoon@users.noreply.github.com>
2021-06-22 18:00:39 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,56 +1,38 @@
 import pyupbit
 import time
 
-access = ""          # 본인 값으로 변경
-secret = ""          # 본인 값으로 변경
+access = ""
+secret = ""
 upbit = pyupbit.Upbit(access, secret)
 
-a = upbit.get_order("KRW-ETH")[0]["uuid"] # 매수 UUID
-b = upbit.get_order("KRW-ETH")[1]["uuid"] # 매도 UUID
+a = upbit.get_order("KRW-ETH")[0]["uuid"]
+b = upbit.get_order("KRW-ETH")[1]["uuid"]
 
 try:
     while True:
-        #c = len(upbit.get_order("KRW-ETH")) #미체결 건 수
         if 1 == len(upbit.get_order("KRW-ETH")):
             if a == upbit.get_order("KRW-ETH")[0]["uuid"]:
-                #print("a에 들어옴")
-                upbit.cancel_order(a)
-                g = float(upbit.get_order(b)["trades"][0]["price"]) # 매도 금액
-                e = round(g * 1.00266666667, -3) # 매도가격
-                h = 6000/g # 매도주문수량
-                i = round(g / 1.00266666667, -3) # 매수가격
-                j = 6000/i # 매수주문수량
+                g = float(upbit.get_order(b)["trades"][0]["price"])
+                e = round(g * 1.00266666667, -3)
+                h = 6000/g
+                i = round(g / 1.00266666667, -3)
+                j = 6000/i
                 upbit.sell_limit_order("KRW-ETH", e, h)
                 upbit.buy_limit_order("KRW-ETH", i, j)
-                #time.sleep(0.001)
-                #if 2 == len(upbit.get_order("KRW-ETH")):
-                a = upbit.get_order("KRW-ETH")[0]["uuid"] # 매수 UUID
-                b = upbit.get_order("KRW-ETH")[1]["uuid"] # 매도 UUID
-                    #print("uuid 2개")
-                #else:
-                #a = upbit.get_order("KRW-ETH")[0]["uuid"] # 매수 UUID
-                #b = upbit.get_order("KRW-ETH", state="done")[0]["uuid"]
-                    #print("바로 체결?")
+                upbit.cancel_order(a)
+                a = upbit.get_order("KRW-ETH")[0]["uuid"]
+                b = upbit.get_order("KRW-ETH")[1]["uuid"]
             elif b == upbit.get_order("KRW-ETH")[0]["uuid"]:
-                #print("b에 들어옴")
-                upbit.cancel_order(b)
-                f = float(upbit.get_order(a)["trades"][0]["price"]) # 매수 금액
-                k = round(f * 1.00266666667, -3) # 매도가격
-                l = 6000/f # 매도주문수량
-                n = round(f / 1.00266666667, -3) # 매수가격
-                m = 6000/n # 매수주문수량
+                f = float(upbit.get_order(a)["trades"][0]["price"])
+                k = round(f * 1.00266666667, -3)
+                l = 6000/f
+                n = round(f / 1.00266666667, -3)
+                m = 6000/n
                 upbit.sell_limit_order("KRW-ETH", k, l)
                 upbit.buy_limit_order("KRW-ETH", n, m)
-                #time.sleep(0.001)
-                #if 2 == len(upbit.get_order("KRW-ETH")):
-                a = upbit.get_order("KRW-ETH")[0]["uuid"] # 매수 UUID
-                b = upbit.get_order("KRW-ETH")[1]["uuid"] # 매도 UUID
-                    #print("uuid 2개")
-                #else:
-                #a = upbit.get_order("KRW-ETH")[0]["uuid"] # 매수 UUID
-                #b = upbit.get_order("KRW-ETH", state="done")[0]["uuid"]
-                    #print("바로 체결?")
-        #time.sleep(0.001)
+                upbit.cancel_order(b)
+                a = upbit.get_order("KRW-ETH")[0]["uuid"]
+                b = upbit.get_order("KRW-ETH")[1]["uuid"]
 except:
-    a = upbit.get_order("KRW-ETH")[0]["uuid"] # 매수 UUID
+    a = upbit.get_order("KRW-ETH")[0]["uuid"]
     b = upbit.get_order("KRW-ETH", state="done")[0]["uuid"]
