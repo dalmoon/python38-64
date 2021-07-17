6ee577c3083da78b18a8cf19a16b31634e6101e2
dalmoon <34093146+dalmoon@users.noreply.github.com>
2021-07-17 19:53:03 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import pyupbit
 access = ""
 secret = ""
 upbit = pyupbit.Upbit(access, secret)
-c = float(upbit.get_order(b)["trades"][0]["price"])
+c = upbit.get_order("KRW-ETH", state="done")[0]["price"]
 
 while True:
     if 1 == len(upbit.get_order("KRW-ETH")):
         a = upbit.get_order("KRW-ETH")[0]["uuid"]
-        if a != upbit.get_order("KRW-ETH", state="done")[0]["uuid"] and c < float(upbit.get_order(b)["trades"][0]["price"]):
+        if a != upbit.get_order("KRW-ETH", state="done")[0]["uuid"] and c < upbit.get_order("KRW-ETH", state="done")[0]["price"]:
             b = upbit.get_order("KRW-ETH", state="done")[0]["uuid"]
             g = float(upbit.get_order(b)["trades"][0]["price"])
             e = round(g * 1.00266666667, -3)
