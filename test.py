6b0774a250f4a3d5fe44c73ff3106aca99d8d373
dalmoon <34093146+dalmoon@users.noreply.github.com>
2021-07-22 15:38:43 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,17 +7,7 @@ upbit = pyupbit.Upbit(access, secret)
 while True:
     if 1 == len(upbit.get_order("KRW-BTC")):
         a = upbit.get_order("KRW-BTC")[0]["uuid"]
-        if a != upbit.get_order("KRW-BTC", state="done")[0]["uuid"] and 'bid' == upbit.get_order("KRW-BTC", state="done")[0]["side"]:
-            b = upbit.get_order("KRW-BTC", state="done")[0]["uuid"]
-            g = float(upbit.get_order(b)["trades"][0]["price"])
-            e = round(g * 1.00266666667, -3)
-            h = 6000/g
-            i = round(g / 1.00266666667, -3)
-            j = 15600/i
-            upbit.sell_limit_order("KRW-BTC", e, h)
-            upbit.buy_limit_order("KRW-BTC", i, j)
-            upbit.cancel_order(a)
-        elif a != upbit.get_order("KRW-BTC", state="done")[0]["uuid"]:
+        if a != upbit.get_order("KRW-BTC", state="done")[0]["uuid"]:
             b = upbit.get_order("KRW-BTC", state="done")[0]["uuid"]
             g = float(upbit.get_order(b)["trades"][0]["price"])
             e = round(g * 1.00266666667, -3)
