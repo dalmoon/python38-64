27e5ef36c51458102356af704478479679ada099
dalmoon <34093146+dalmoon@users.noreply.github.com>
2021-07-22 14:15:20 +0900

    Update test.py

--- test.py
+++ test.py
@@ -17,6 +17,16 @@ while True:
             upbit.sell_limit_order("KRW-BTC", e, h)
             upbit.buy_limit_order("KRW-BTC", i, j)
             upbit.cancel_order(a)
+        elif a != upbit.get_order("KRW-BTC", state="done")[0]["uuid"]:
+            b = upbit.get_order("KRW-BTC", state="done")[0]["uuid"]
+            g = float(upbit.get_order(b)["trades"][0]["price"])
+            e = round(g * 1.00266666667, -3)
+            h = 6000/g
+            i = round(g / 1.00266666667, -3)
+            j = 6000/i
+            upbit.sell_limit_order("KRW-BTC", e, h)
+            upbit.buy_limit_order("KRW-BTC", i, j)
+            upbit.cancel_order(a)
         elif a == upbit.get_order("KRW-BTC", state="done")[0]["uuid"]:
             b = upbit.get_order("KRW-BTC", state="done")[1]["uuid"]
             f = float(upbit.get_order(b)["trades"][0]["price"])
