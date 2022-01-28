b1db484868c288458e0911d9f38fd1885d03e42d
dalmoon <34093146+dalmoon@users.noreply.github.com>
2022-01-29 02:36:43 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,8 +6,9 @@ secret = "MDsSEcUEdsxUH8dXpIqBNlCGffbPNeYVsoxwO4cp"
 upbit = pyupbit.Upbit(access, secret)
 
 while True:
-    time.sleep(0.001)
+time.sleep(0.0001)
     if 1 == len(upbit.get_order("KRW-ETH")):
+    time.sleep(0.0001)
         a = upbit.get_order("KRW-ETH")[0]["uuid"]
         if a != upbit.get_order("KRW-ETH", state="done")[0]["uuid"]:
             b = upbit.get_order("KRW-ETH", state="done")[0]["uuid"]
@@ -29,4 +30,4 @@ while True:
             upbit.sell_limit_order("KRW-ETH", k, l)
             upbit.buy_limit_order("KRW-ETH", n, m)
             upbit.cancel_order(a)
-        time.sleep(0.001)
+        time.sleep(0.0001)
