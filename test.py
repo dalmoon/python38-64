7516a11118cb6ec2b283f1f87bcae417de59617e
dalmoon <34093146+dalmoon@users.noreply.github.com>
2022-02-02 16:34:57 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ secret = ""
 upbit = pyupbit.Upbit(access, secret)
 
 while True:
-    time.sleep(0.264)
+    time.sleep(0.500)
     if 1 == len(upbit.get_order("KRW-ETH")):
         time.sleep(0.300)
         a = upbit.get_order("KRW-ETH")[0]["uuid"]
