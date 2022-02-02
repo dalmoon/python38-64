1edb824fa3c2f3251cc8301bb9fd3dc0e33d94ef
dalmoon <34093146+dalmoon@users.noreply.github.com>
2022-02-02 16:44:03 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import time
 import pyupbit
 
-access = "3Ml3Q1jpetQZnpchZGWt6IHgxrO0Qd0uovTdxt4E"
-secret = "Ew0MVPGOUQAtQsvvrCW36pDFqPTSwiu3xhB4elbT"
+access = ""
+secret = ""
 upbit = pyupbit.Upbit(access, secret)
 
 while True:
